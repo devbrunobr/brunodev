e776e47c260ca7cfdc09f6244a2c462a2d3d0f8a
devbrunobr <ouvidoriabrunocesar@gmail.com>
2025-04-20 20:31:23 -0300

    Create whatsapp.js

---
+++ whatsapp.js
@@ -0,0 +1,6 @@
+// Botão flutuante de WhatsApp
+const whatsappBtn = document.createElement('a');
+whatsappBtn.href = "https://wa.me/5513997948067";
+whatsappBtn.className = "whatsapp-float";
+whatsappBtn.innerHTML = '<i class="fab fa-whatsapp"></i>';
+document.body.appendChild(whatsappBtn);
